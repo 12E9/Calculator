a7a3737e4c588f50f42eab53f6d56cb1f4edf059
Your Name <you@example.com>
2022-02-22 05:51:22 -0600

    added

---
+++ Script10.js
@@ -0,0 +1,15 @@
+function d(val){
+    document.getElementById('result').value += val;
+
+}
+function solve(){
+    var value1 = document.getElementById('result').value;
+    let res = eval(value1);
+    document.getElementById('result').value = res;
+
+
+}
+function cleard(){
+    document.getElementById('result').value="";
+
+}
